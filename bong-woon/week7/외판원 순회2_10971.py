b6a08b15fe6e893db009e8a858e0cc4d212a07be
bongwoon.baek <qhddns529@gmail.com>
2024-06-24 19:19:28 +0900

    코드 수정

--- bong-woon/week7/외판원 순회2_10971.py
+++ bong-woon/week7/외판원 순회2_10971.py
@@ -3,7 +3,7 @@ n = int(input())
 info = [list(map(int, input().split())) for _ in range(n)]
 lst = []
 
-def travel(x, start, check, lst, dist, count):
+def travel(x, start, check, lst, dist):
     if sum(check) == n and info[x][start] != 0:
         lst.append(dist + info[x][start])
         return lst
@@ -12,13 +12,12 @@ def travel(x, start, check, lst, dist, count):
     for i in range(n):
         if check[i] == False and info[x][i] != 0:
             check[i] = True
-            travel(i, start, check, lst, dist + info[x][i], count + 1)
+            travel(i, start, check, lst, dist + info[x][i])
             check[i] = False
 
 for num in range(n):
     check = [False] * n
     dist = 0
-    count = 0
-    travel(num, num, check, lst, dist, count)
+    travel(num, num, check, lst, dist)
 
 print(min(lst))
