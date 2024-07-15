1caefac56c0b83a645fe2bc57011445fc646e8d1
ZZOMING-K <89764752+ZZOMING-K@users.noreply.github.com>
2024-07-15 15:53:25 +0900

    Delete chohj01 directory

--- chohj01/README.md
+++
@@ -1,5 +0,0 @@
-First week
-
-Greedy Algorithm
-
-
--- chohj01/week1/30.py
+++
@@ -1,8 +0,0 @@
-number = list(map( int, input()))
-number.sort(reverse = True)
-
-if sum(number)%3 == 0 and number[-1] == 0:
-    a = ("".join(map(str, number)))
-    print(a)
-else:
-    print("-1")
